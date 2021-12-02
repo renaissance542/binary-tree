81dc0d07aa6ce326002539b0e715f2d481646c6a
Philippe Roy <renaissance542@gmail.com>
2021-12-02 15:52:23 -0500

    completed methods: level_order, preorder, inorder, postorder, to_a, and height

--- main.rb
+++ main.rb
@@ -4,14 +4,65 @@ require_relative 'tree'
 
 system("clear") || system("cls")
 
-arr = []
-10.times { arr.push rand 100 }
+# arr = []
+# 20.times { arr.push rand 50 }
+
+arr = [0, 13, 15, 19, 21, 27, 30, 31, 32, 34, 35, 39, 40, 42, 45, 47, 48, 49, 50, 51, 52]
+
 p arr.sort
 
 tree = Tree.new(arr)
-tree.pretty_print
+# tree.pretty_print
 
 puts "\n*********************\n\n"
-tree.insert(40)
+tree.insert(27)
+tree.insert(50)
+tree.insert(51)
+tree.insert(52)
 tree.pretty_print
 
+# p tree.find(27)
+# puts "\n*********************\n\n"
+# tree.delete(27)
+# tree.pretty_print
+
+p tree.to_a
+
+# puts "\nlevel_order { |n| puts n.data }"
+# tree.level_order { |n| puts n.data }
+
+# puts "\nlevel_order"
+# p tree.level_order
+
+# puts "\ninorder { |n| p n.data }"
+# tree.inorder { |n| p n.data }
+
+# puts "\nprenorder { |n| p n.data }"
+# tree.preorder { |n| p n.data }
+
+# puts "\npostorder { |n| p n.data }"
+# tree.postorder { |n| p n.data }
+
+puts "\ntree.height"
+p tree.height
+
+puts "\ntree.height(tree.root.left)"
+p tree.height(tree.root.left)
+
+puts "\ntree.height(tree.find(27))"
+p tree.height(tree.find(27))
+
+puts "\ntree.depth(tree.find(27))"
+p tree.depth(tree.find(27))
+
+puts "\ntree.height(tree.find(47))"
+p tree.height(tree.find(47))
+
+puts "\ntree.depth(tree.find(47))"
+p tree.depth(tree.find(47))
+
+puts "\ntree.height(tree.find(46)) *missing node*"
+p tree.height(tree.find(46))
+
+puts "\ntree.depth(tree.find(46)) *missing node*"
+p tree.depth(tree.find(46))
--- tree.rb
+++ tree.rb
@@ -2,6 +2,9 @@
 
 require_relative 'node'
 require 'pry-byebug'
+
+# rubocop:disable Metric/ClassLength
+
 # binary tree class
 class Tree
   attr_accessor :root
@@ -24,9 +27,99 @@ class Tree
     insert_leaf(@root, value)
   end
 
-  def delete(data)
+  def delete(value)
+    find_target_nodes(value)
+    return if @target.nil?
+
+    case count_children(@target)
+    when 0
+      delete_target_no_child
+    when 1
+      delete_target_one_child
+    when 2
+      delete_target_two_childs
+    end
   end
 
+  def find(value, node = @root)
+    return node if node.nil? || node.data == value
+
+    if value < node.data
+      find(value, node.left)
+    else
+      find(value, node.right)
+    end
+  end
+
+  def level_order(nodes = [@root], &block)
+    return to_a unless block_given? # array of values
+
+    node = nodes.shift
+    nodes.push node.left unless node.left.nil?
+    nodes.push node.right unless node.right.nil?
+    yield node
+    level_order(nodes, &block) unless nodes.empty?
+  end
+
+  def inorder(node = @root, &block)
+    return to_a unless block_given? # array of values
+
+    inorder(node.left, &block) unless node.left.nil?
+    yield node
+    inorder(node.right, &block) unless node.right.nil?
+  end
+
+  def preorder(node = @root, &block)
+    return to_a unless block_given? # array of values
+
+    yield node
+    preorder(node.left, &block) unless node.left.nil?
+    preorder(node.right, &block) unless node.right.nil?
+  end
+
+  def postorder(node = @root, &block)
+    return to_a unless block_given? # array of values
+
+    postorder(node.left, &block) unless node.left.nil?
+    postorder(node.right, &block) unless node.right.nil?
+    yield node
+  end
+
+  # return array of all values in the tree
+  def to_a(node = @root)
+    arr = []
+    arr += to_a(node.left) unless node.left.nil?
+    arr += [node.data]
+    arr += to_a(node.right) unless node.right.nil?
+    arr
+  end
+
+  # accepts a node and returns its height
+  def height(node = @root)
+    return -1 if node.nil?
+
+    1 + [height(node.left), height(node.right)].max
+  end
+
+  # accepts a node and returns its depth
+  def depth(node)
+    return height unless node
+
+    current = @root
+    depth = 0
+    while current.data != node.data && current
+      depth += 1
+      current = node.data < current.data ? current.left : current.right
+    end
+    depth
+  end
+
+  # the difference between heights of left subtree
+  # and right subtree of every node is not more than 1
+  def balanced?
+
+  end
+  
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -34,7 +127,73 @@ class Tree
   end
 
   private
-  
+
+  def count_children(node)
+    children = 2
+    children -= 1 if node.left.nil?
+    children -= 1 if node.right.nil?
+    children
+  end
+
+  def delete_target_no_child
+    if @target.data < @parent.data
+      @parent.left = nil
+    else
+      @parent.right = nil
+    end
+  end
+
+  # attach the deleted node's child to the correct position on the parent
+  def delete_target_one_child
+    if @parent.nil?
+      @root = @target.left.nil? ? @target.right : @target.left
+    elsif @target.data < @parent.data
+      @parent.left = @target.left.nil? ? @target.right : @target.left
+    else
+      @parent.right = @target.left.nil? ? @target.right : @target.left
+    end
+  end
+
+  def delete_target_two_childs
+    find_inorder_nodes
+    fix_parent_children
+    @inorderparent.left = @inorder.right unless @inorderparent == @target
+    @inorder.left = @target.left
+    @inorder.right = @target.right unless @inorderparent == @target
+  end
+
+  def fix_parent_children
+    if @parent.nil?
+      @root = @inorder
+    elsif @target.data < @parent.data
+      @parent.left = @inorder
+    else
+      @parent.right = @inorder
+    end
+  end
+
+  def find_target_nodes(value)
+    @parent = nil
+    @target = @root
+    while @target.data != value && !@target.nil?
+      @parent = @target
+      @target = next_child(value)
+    end
+  end
+
+  def find_inorder_nodes
+    @inorderparent = @target
+    @inorder = @target.right
+    until @inorder.left.nil?
+      @inorderparent = @inorder
+      @inorder = @inorder.left
+    end
+  end
+
+  def next_child(value)
+    value < @target.data ? @target.left : @target.right
+  end
+
   # rubocop:disable Metrics/MethodLength
   def insert_leaf(node, value)
     return if value == node.data
@@ -45,12 +204,11 @@ class Tree
       else
         insert_leaf(node.left, value)
       end
+    elsif node.right.nil?
+      node.right = Node.new(value)
     else
-      if node.right.nil?
-        node.right = Node.new(value)
-      else
-        insert_leaf(node.right, value)
-      end
+      insert_leaf(node.right, value)
     end
   end
+  # rubocop:enable Metrics/MethodLength
 end
